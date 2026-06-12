c9910e750174555f907eb9ca7f7839ee662ec4f6
shreyawatts <priyadarsinidas.usa2@gmail.com>
2026-06-12 11:41:40 -0400

    comments in python

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print("hello world")
 print("my name is Shreyanshi")
 print("I like to play volleyball")
+#print keyword is used to output the sentnce
 print("the sum of 3 and 5 is:",3+5)
 print(5*8)
